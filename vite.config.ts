4036d5230c816718c989f6e03d66560ffaf5073f
Arkhipov <Drunka-3@yandex.ru>
2025-06-25 14:59:56 +0300

    add gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -17,6 +17,9 @@ export default defineConfig({
     Components({
       resolvers: [ElementPlusResolver()],
     }),],
+  base: process.env.NODE_ENV === 'production'
+    ? '/workout_tracker/'
+    : '/',
   server: {
     port: 5173
   },
